8f135c474e982632086c5a8a8a36aac4bcc12c55
joaomps <joaomps@outlook.pt>
2016-05-16 11:20:06 +0100

    Update Lugar.java

--- Lugar.java
+++ Lugar.java
@@ -1,15 +1,50 @@
 package Projeto8;
 
 public class Lugar {
-
-	public String obterNumero() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private int numeroLugar;
+	private int escalao;
+	
+	public Lugar(int numero, int escalao){
+		this.numeroLugar = numero;
+		this.escalao = escalao;
 	}
 
-	public String obterEscalao() {
-		// TODO Auto-generated method stub
-		return null;
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + escalao;
+		result = prime * result + numeroLugar;
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Lugar other = (Lugar) obj;
+		if (escalao != other.escalao) {
+			return false;
+		}
+		if (numeroLugar != other.numeroLugar) {
+			return false;
+		}
+		return true;
+	}
+
+	public int obterNumero() {
+		return numeroLugar;
+	}
+
+	public int obterEscalao() {
+		return escalao;
 	}
 	
 }
